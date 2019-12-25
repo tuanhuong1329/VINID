a4d5f8c4e56e6e03e2a57a05b857a582d9718208
th <th@ths-MacBook-Pro.net.fpt>
2019-12-25 16:15:33 +0000

    Homework Day 1

---
+++ HomeWorkDay1/HomeWorkDay1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HomeWorkDay1
+//
+//  Created by th on 12/25/19.
+//  Copyright © 2019 th. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
